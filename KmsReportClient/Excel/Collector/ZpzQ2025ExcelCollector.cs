1b61da74d12cf2b6cc02941ac5e00a7e93961424
modorugg <mo.do.ru.gg@gmail.com>
2025-01-20 15:43:29 +0800

    fixed zpzQ2025excelcollector

--- KmsReportClient/Excel/Collector/ZpzQ2025ExcelCollector.cs
+++ KmsReportClient/Excel/Collector/ZpzQ2025ExcelCollector.cs
@@ -9,32 +9,28 @@ namespace KmsReportClient.Excel.Collector
 {
     class ZpzQ2025ExcelCollector : ExcelBaseCollector
     {
-        private readonly string[] _columnsTable5A = { "2", "4", "5", "6", "7", "8", "9" };
-        private readonly string[] _columnsTable6 = { "2", "4", "5", "6", "7", "8", "9", "11", "12", "13", "14", "15", "16" };
-        private readonly string[] _columnsTable7 = { "2", "4", "5", "6", "7", "8", "9", "11", "12", "13", "14", "15", "16" };
-        private readonly string[] _columnsTable8 = { "2", "5" };
-        private readonly string[] _columnsTable9 = { "2", "7", "9" };
-        private readonly string[] _columnsTableLetal1 = { "2", "3","4","5","6","7" };
+        private readonly string[] _columnsTable6 = { "", "4", "5", "6", "7", "8", "9", "11", "12", "13", "14", "15", "16" };
+        private readonly string[] _columnsTable7 = { "", "4", "5", "6", "7", "8", "9", "11", "12", "13", "14", "15", "16" };
+        private readonly string[] _columnsTable8 = { "", "11", "12", "13", "14", "15", "16" };
+        private readonly string[] _columnsTable9 = { "", "7", "9" };
 
         protected override void FillReport(string form, AbstractReport destReport, AbstractReport srcReport)
         {
-            var destData = (destReport as ReportZpz2025)?.ReportDataList.Single(r => r.Theme == form) ?? 
+            var destData = (destReport as ReportZpz2025)?.ReportDataList.Single(r => r.Theme == form) ??
                            throw new Exception($"Can't find destReportDataList for form = {form}");
-            var srcData = (srcReport as ReportZpz2025)?.ReportDataList.Single(r => r.Theme == form) ?? 
+            var srcData = (srcReport as ReportZpz2025)?.ReportDataList.Single(r => r.Theme == form) ??
                           throw new Exception($"Can't find srcReportDataList for form = {form}");
             destData.Data = srcData.Data;
         }
 
         protected override AbstractReport CollectReportData(string form)
         {
-            var themeData = form switch {
+            var themeData = form switch
+            {
                 "Таблица 6" => FillTable6(form),
-                "Таблица 7" => FillTable6(form),
-                "Таблица 8" => FillTable1(form),
-                "Таблица 9" => FillTable4(form),
-                "Таблица 1Л" => FillTableLetal(form),
-                "Таблица 2Л" => FillTableLetal(form),
-                _ => FillTable5()
+                "Таблица 7" => FillTable7(form),
+                "Таблица 8" => FillTable8(form),
+                "Таблица 9" => FillTable9(form),
             };
             var report = new ReportZpz2025 { ReportDataList = new ReportZpz2025Dto[1] };
             report.ReportDataList[0] = new ReportZpz2025Dto
@@ -45,47 +41,7 @@ namespace KmsReportClient.Excel.Collector
             return report;
         }
 
-        private ReportZpz2025DataDto[] FillTable4(string form)
-        {
-            var list = new List<ReportZpz2025DataDto>();
-            int countWorkSheet = ObjWorkBook.Worksheets.Count;
-            int startList = form == "Таблица 1" ? 2 : 1;
-
-            for (int currentList = startList; currentList <= countWorkSheet; currentList++)
-            {
-                ObjWorkSheet = (Worksheet)ObjWorkBook.Sheets[currentList];
-
-                if (!ObjWorkSheet.Name.ToLower().Contains("Pag".ToLower()))
-                    continue;
-                int lastRow = GetLastRow();
-
-                Dictionary<string, int> dictionary;
-                int startRow;
-                string rowDataIndexFirst;
-                string rowDataIndexSecond;
-                startRow = GetStartRow();
-               
-                //startRow = 16;
-                rowDataIndexFirst = "7";
-                rowDataIndexSecond = "9";
-                dictionary = FindColumnIndexies(_columnsTable9, startRow - 1);
-               
-                for (int i = startRow; i <= lastRow; i++)
-                {
-                    var data = new ReportZpz2025DataDto
-                    {
-                        Code = ObjWorkSheet.Cells[i, dictionary["2"]].Value,
-                        CountSmo = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary[rowDataIndexFirst]].Value),
-                        CountSmoAnother = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary[rowDataIndexSecond]].Value) 
-                    };
-                    list.Add(data); 
-                }
-            }
-
-            return list.ToArray();
-        }
-
-        private ReportZpz2025DataDto[] FillTable5()
+        private ReportZpz2025DataDto[] FillTable9(string form)
         {
             var list = new List<ReportZpz2025DataDto>();
             int countWorkSheet = ObjWorkBook.Worksheets.Count;
@@ -94,20 +50,18 @@ namespace KmsReportClient.Excel.Collector
             {
                 ObjWorkSheet = (Worksheet)ObjWorkBook.Sheets[currentList];
                 int lastRow = GetLastRow();
-                int startRow = GetStartRow(); //currentList == 1 ? 16 : 5;
-                Dictionary<string, int> dictionary = FindColumnIndexies(_columnsTable5A, startRow - 1);
+
+                Dictionary<string, int> dictionary;
+                int startRow = currentList == 1 ? 12 : 5;
+                dictionary = FindColumnIndexies(_columnsTable9, startRow - 1);
 
                 for (int i = startRow; i <= lastRow; i++)
                 {
                     var data = new ReportZpz2025DataDto
                     {
-                        Code = ObjWorkSheet.Cells[i, dictionary["2"]].Value,
-                        CountOutOfSmo = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["4"]].Value),
-                        CountAmbulatory = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["5"]].Value),
-                        CountDs = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["6"]].Value),
-                        CountDsVmp = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["7"]].Value),
-                        CountStac = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["8"]].Value),
-                        CountStacVmp = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["9"]].Value)
+                        Code = ObjWorkSheet.Cells[i, dictionary[""]].Value,
+                        CountSmo = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["7"]].Value),
+                        CountSmoAnother = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["9"]].Value)
                     };
                     list.Add(data);
                 }
@@ -116,7 +70,8 @@ namespace KmsReportClient.Excel.Collector
             return list.ToArray();
         }
 
-        private ReportZpz2025DataDto[] FillTable1(string form)
+
+        private ReportZpz2025DataDto[] FillTable8(string form)
         {
             var list = new List<ReportZpz2025DataDto>();
             int countWorkSheet = ObjWorkBook.Worksheets.Count;
@@ -127,9 +82,7 @@ namespace KmsReportClient.Excel.Collector
                 int lastRow = GetLastRow();
 
                 Dictionary<string, int> dictionary;
-                int startRow = GetStartRow();
-
-                //startRow = currentList == 1 ? 15 : 4;
+                int startRow = currentList == 1 ? 11 : 5;
                 dictionary = FindColumnIndexies(_columnsTable8, startRow - 1);
 
 
@@ -137,8 +90,13 @@ namespace KmsReportClient.Excel.Collector
                 {
                     var data = new ReportZpz2025DataDto
                     {
-                        Code = ObjWorkSheet.Cells[i, dictionary["2"]].Value,
-                        CountSmo = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["5"]].Value)
+                        Code = ObjWorkSheet.Cells[i, dictionary[""]].Value,
+                        CountOutOfSmoAnother = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["11"]].Value),
+                        CountAmbulatoryAnother = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["12"]].Value),
+                        CountDsAnother = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["13"]].Value),
+                        CountDsVmpAnother = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["14"]].Value),
+                        CountStacAnother = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["15"]].Value),
+                        CountStacVmpAnother = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["16"]].Value),
                     };
                     list.Add(data);
                 }
@@ -156,16 +114,14 @@ namespace KmsReportClient.Excel.Collector
             {
                 ObjWorkSheet = (Worksheet)ObjWorkBook.Sheets[currentList];
                 int lastRow = GetLastRow();
-                int startRow = GetStartRow(); //currentList == 1 ? 16 : 5;
-                Dictionary<string, int> dictionary = form == "Таблица 2" ?
-                   FindColumnIndexies(_columnsTable6, startRow - 1) :
-                   FindColumnIndexies(_columnsTable7, startRow - 1);
+                int startRow = currentList == 1 ? 11 : 5;
+                Dictionary<string, int> dictionary = FindColumnIndexies(_columnsTable6, startRow - 1);
 
                 for (int i = startRow; i <= lastRow; i++)
                 {
                     var data = new ReportZpz2025DataDto
                     {
-                        Code = ObjWorkSheet.Cells[i, dictionary["2"]].Value,
+                        Code = ObjWorkSheet.Cells[i, dictionary[""]].Value,
                         CountOutOfSmo = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["4"]].Value),
                         CountAmbulatory = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["5"]].Value),
                         CountDs = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["6"]].Value),
@@ -186,7 +142,8 @@ namespace KmsReportClient.Excel.Collector
             return list.ToArray();
         }
 
-        private ReportZpz2025DataDto[] FillTableLetal(string form)
+
+        private ReportZpz2025DataDto[] FillTable7(string form)
         {
             var list = new List<ReportZpz2025DataDto>();
             int countWorkSheet = ObjWorkBook.Worksheets.Count;
@@ -195,36 +152,26 @@ namespace KmsReportClient.Excel.Collector
             {
                 ObjWorkSheet = (Worksheet)ObjWorkBook.Sheets[currentList];
                 int lastRow = GetLastRow();
-
-                Dictionary<string, int> dictionary = new Dictionary<string, int>();
-                int startRow = 0;
-
-                switch (form)
-                {
-                    case "Таблица 1Л":
-                        startRow = GetStartRow();
-                        lastRow = 35;
-                        dictionary = FindColumnIndexies(_columnsTableLetal1, startRow - 1);
-                        break;
-                    case "Таблица 2Л":
-                        startRow = GetStartRow();
-                        lastRow = 45;
-                        dictionary = FindColumnIndexies(_columnsTableLetal1, startRow - 1);
-                        break;
-
-                }
+                int startRow = currentList == 1 ? 11 : 5;
+                Dictionary<string, int> dictionary = FindColumnIndexies(_columnsTable7, startRow - 1);
 
                 for (int i = startRow; i <= lastRow; i++)
                 {
                     var data = new ReportZpz2025DataDto
                     {
-                        Code = ObjWorkSheet.Cells[i, dictionary["2"]].Value,                    
-                        CountAmbulatory = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["3"]].Value),
-                        CountStac = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["4"]].Value),
-                        CountDs = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["5"]].Value),
-                        CountOutOfSmoAnother = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["6"]].Value),
-                        CountSmo = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["7"]].Value),
-                      
+                        Code = ObjWorkSheet.Cells[i, dictionary[""]].Value,
+                        CountOutOfSmo = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["4"]].Value),
+                        CountAmbulatory = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["5"]].Value),
+                        CountDs = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["6"]].Value),
+                        CountDsVmp = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["7"]].Value),
+                        CountStac = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["8"]].Value),
+                        CountStacVmp = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["9"]].Value),
+                        CountOutOfSmoAnother = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["11"]].Value),
+                        CountAmbulatoryAnother = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["12"]].Value),
+                        CountDsAnother = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["13"]].Value),
+                        CountDsVmpAnother = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["14"]].Value),
+                        CountStacAnother = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["15"]].Value),
+                        CountStacVmpAnother = GlobalUtils.TryParseDecimal(ObjWorkSheet.Cells[i, dictionary["16"]].Value),
                     };
                     list.Add(data);
                 }
@@ -232,6 +179,5 @@ namespace KmsReportClient.Excel.Collector
 
             return list.ToArray();
         }
-
     }
 }
