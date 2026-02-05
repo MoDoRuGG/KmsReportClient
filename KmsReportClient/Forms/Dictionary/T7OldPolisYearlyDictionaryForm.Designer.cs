6ec9627ac5139002db476ff3dd4655147439ded4
modorugg <mo.do.ru.gg@gmail.com>
2026-02-05 15:27:02 +0800

    T7DictForm fix

--- KmsReportClient/Forms/Dictionary/T7OldPolisYearlyDictionaryForm.Designer.cs
+++ KmsReportClient/Forms/Dictionary/T7OldPolisYearlyDictionaryForm.Designer.cs
@@ -136,7 +136,7 @@ namespace KmsReportClient.Forms.Dictionary
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(500, 581);
+            this.ClientSize = new System.Drawing.Size(1246, 581);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.dgvDictionary);
