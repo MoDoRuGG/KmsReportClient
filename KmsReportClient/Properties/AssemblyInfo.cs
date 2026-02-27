5eabd22602ae37e211a960bb0b8a3166dd59eb77
modorugg <mo.do.ru.gg@gmail.com>
2026-02-27 13:11:19 +0800

    release 1.1.9.09

--- KmsReportClient/Properties/AssemblyInfo.cs
+++ KmsReportClient/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.1.9.08")]
-[assembly: AssemblyFileVersion("1.1.9.08")]
+[assembly: AssemblyVersion("1.1.9.09")]
+[assembly: AssemblyFileVersion("1.1.9.09")]
